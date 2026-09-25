9ea365edd701fb5c51e1f0596f96d1ee21f06851
tauqeerkhan199 <ktauqeer345@gmail.com>
2026-09-25 18:44:56 +0530

    second commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     });
 
     res.end(`
-        <h1>Hello this is ${APP_ENV} env ITCAMPUSGURU by tauqeer </h1>
+        <h1>Hello this is ${APP_ENV} env ITCAMPUSGURU by tauqeer sir </h1>
         <p>Application deployed using GitHub Actions → AWS EC2</p>
         <p>Environment: <strong>${APP_ENV}</strong></p>
     `);
